b48675c1da135c4ce0ddd039a04338c77798add4
jollyjpg <zacharymgamer007@gmail.com>
2025-02-01 19:28:11 -0500

    infinity functionality
    float.positiveinfinity

--- obj/Debug/netstandard2.1/JamesLevelThreat.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/JamesLevelThreat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JamesLevelThreat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4e3b54fe6992ddd2a4d29e2cc2ab574febe1182")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0de64637e614e87f51d41075806c33491eea4728")]
 [assembly: System.Reflection.AssemblyProductAttribute("JamesLevelThreat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JamesLevelThreat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
