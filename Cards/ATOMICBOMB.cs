558272910993a426370d3f218b8c22cc479448b3
jollyjpg <zacharymgamer007@gmail.com>
2025-02-02 17:43:46 -0500

    my brain hurts

---
+++ Cards/ATOMICBOMB.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+using RarityLib;
+using RarityLib.Utils;
+using ModsPlus;
+using SimulationChamber;
+
+
+namespace JamesLevelThreat.Cards
+{
+    public class ATOMICBOMB : CustomEffectCard<DestructionEffect>
+    {
+        public override CardDetails Details => new CardDetails
+        {
+            Title = "ATOMIC BOMB",
+            Description = "Your imminent demise.",
+            Rarity = RarityUtils.GetRarity("Honored"),
+            //or use RarityUtils.GetRarity("insert") if custom rarity
+            Theme = CardThemeColor.CardThemeColorType.DestructiveRed,
+        };
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+
+        }
+
+        protected override void Added(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            Debug.Log("Card added to the player!");
+        }
+
+        protected override void Removed(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            Debug.Log("Card removed from the player!");
+        }
+    }
+
+    public class DestructionEffect : CardEffect
+    {
+        public override void OnBlock(BlockTrigger.BlockTriggerType trigger)
+        {
+            Debug.Log("[ExampleEffect] Player blocked!");
+            var gun = new GameObject("gun").AddComponent<SimulatedGun>();
+            gun.numberOfProjectiles = 1;
+            gun.bursts = 0;
+            gun.spread = 0f;
+            gun.evenSpread = 0f;
+            gun.damage = 100;
+            gun.SimulatedAttack(this.player.playerID, new Vector3(0, 0, 0), new Vector3(0, -10, 0), 0, 1.5f);
+        }
+        
+        public override void OnShoot(GameObject projectile)
+        {
+            Debug.Log("[ExampleEffect] Player fired a shot!");
+        }
+
+    }
+
+}
--- obj/Debug/netstandard2.1/JamesLevelThreat.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/JamesLevelThreat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JamesLevelThreat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bb472b27cdc3b94c72243579699cf43215c04c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9fc9ae77e8bc2eb1f797360c41e61cc086682c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("JamesLevelThreat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JamesLevelThreat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
