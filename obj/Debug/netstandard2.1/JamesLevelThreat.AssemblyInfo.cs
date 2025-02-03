ce3d629a03ef8ecc8414e562020f0abc8d22e25d
jollyjpg <zacharymgamer007@gmail.com>
2025-02-03 17:28:10 -0500

    forgot to build

--- obj/Debug/netstandard2.1/JamesLevelThreat.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/JamesLevelThreat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JamesLevelThreat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af35a81dfa53fcc952f768de61b49c53f7134db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75972424f7bae978dc2dff510e804e28bb96fd95")]
 [assembly: System.Reflection.AssemblyProductAttribute("JamesLevelThreat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JamesLevelThreat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
